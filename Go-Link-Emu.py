e58021b7249a359653152076218d3502e192d5f6
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2022-01-29 23:36:52 +0300

    Update Go-Link-Emu.py

--- Go-Link-Emu.py
+++ Go-Link-Emu.py
@@ -1,19 +1,9 @@
 import requests
-
 send2 = requests.get
-send = requests.post
-
 URL = input("Ссылка: ")
-
 while True:
-           try:
-               send(URL)
-               print("Запрос отравлен!")
-           except requests.exceptions.ConnectionError:
-                                                      print("Ошибка отправки!")
-
-           try:
-               send2(URL)
-               print("Запрос отравлен!")
-           except requests.exceptions.ConnectionError:
-                                                      print("Ошибка отправки!")
+    try:
+        send(URL)
+        print("Запрос отравлен!")
+    except requests.exceptions.ConnectionError:
+        print("Ошибка отправки!")
